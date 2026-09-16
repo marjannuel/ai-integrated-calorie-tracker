1308fe67f21b2a60a811eb9bb5ab66634e32cd1f
Marjannuel <marjannuelaltalasaturnino@gmail.com>
2026-09-16 23:00:01 +0800

    Added Dashboard page contents

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,39 @@
-import { User, UserRound } from 'lucide-react';
+import { Cake, UserRound, VenusAndMars, Activity } from 'lucide-react';
 
 export default function DashboardPage(){
     return(
         <div className="h-dvh w-full min-w-0 overflow-x-hidden flex flex-col justify-center items-center">
-            <div className="px-2 w-full max-w-md md:max-w-lg">
+            <div className="px-6 w-full max-w-md md:max-w-lg flex flex-col justify-center items-center gap-2">
                 <h1 className="font-mono text-center text-lg md:text-2xl w-full mb-15 md:mb-20">
                     First of all, kindly tell us what is your...
                 </h1>
                 <div className='flex justify-center items-center gap-2'>
-                    <UserRound className='w-4 h-4 md:w-8 md:h-8'/>
+                    <UserRound className='w-6 h-6 md:w-8 md:h-8'/>
+                    <input className='border md:border-2 outline-none rounded-lg py-1 dark:bg-neutral-800 w-44 md:w-64 px-2 text-sm md:text-base font-sans'
+                    placeholder='Name'>
+                    </input>
                 </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <Cake className='w-6 h-6 md:w-8 md:h-8'/>
+                    <input className='border md:border-2 outline-none rounded-lg py-1 dark:bg-neutral-800 w-44 md:w-64 px-2 text-sm md:text-base font-sans'
+                    placeholder='Birthdate'>
+                    </input>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <VenusAndMars className='w-6 h-6 md:w-8 md:h-8'/>
+                    <input className='border md:border-2 outline-none rounded-lg py-1 dark:bg-neutral-800 w-44 md:w-64 px-2 text-sm md:text-base font-sans'
+                    placeholder='Sex'>
+                    </input>
+                </div>
+                <div className='flex justify-center items-center gap-2'>
+                    <Activity className='w-6 h-6 md:w-8 md:h-8'/>
+                    <input className='border md:border-2 outline-none rounded-lg py-1 dark:bg-neutral-800 w-44 md:w-64 px-2 text-sm md:text-base font-sans'
+                    placeholder='Activeness'>
+                    </input>
+                </div>
+                <button className='mt-5 md:mt-7 text-sm md:text-base font-sans border w-52 md:w-76 py-1 md:py-1.5 rounded-2xl bg-amber-500 text-neutral-900 border-amber-500 md:hover:cursor-pointer'>
+                    Confirm
+                </button>
             </div>
             
         </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -126,7 +126,7 @@ export default function LoginPage(){
             {isOpen && (
                 <div className='fixed inset-0 z-50 bg-black/80 flex justify-center items-center'
                 onClick={() =>  setIsOpen(false)}>
-                    <form className='relative z-10 dark:bg-neutral-900 bg-white w-[90%] max-w-md md:max-w-lg border-3 rounded-3xl border-neutral-500 flex flex-col items-center gap-1 pb-5'
+                    <form className='relative z-10 dark:bg-neutral-900 bg-neutral-200 w-[90%] max-w-md md:max-w-lg border-3 rounded-3xl border-white dark:border-neutral-500 flex flex-col items-center gap-1 pb-5'
                     onSubmit={handleRegister}
                     onClick={(e) => e.stopPropagation()}>
                         <div className='w-[95%] pt-1 md:pt-2 flex justify-end items-center'>
@@ -155,7 +155,7 @@ export default function LoginPage(){
                             value={password}
                             onChange={(e) => setPassword(e.currentTarget.value)}
                             ></input>
-                            <button className='absolute right-2 z-10 bg-white dark:bg-neutral-800 p-1 flex justify-center items-center'
+                            <button className='absolute right-2 z-10 bg-neutral-200 dark:bg-neutral-800 p-1 flex justify-center items-center'
                             type='button'
                             onClick={toggleView}>
                                 <Eye className='w-4 h-4 stroke-1 md:stroke-[1.5] md:hover:cursor-pointer'/>
