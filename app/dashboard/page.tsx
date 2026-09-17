d913c12cda84734b9942d282f0a35dcd0921182e
Marjannuel <marjannuelaltalasaturnino@gmail.com>
2026-09-18 05:44:34 +0800

    Added UI elements on dashboard page for mobile viewport width

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,43 @@
 'use client';
 import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { Menu } from 'lucide-react';
 
 export default function DashboardPage(){
-    return(
-        <div>
+    const [currentUserName, setCurrentUserName] = useState('');
 
+    useEffect(() => {
+        const fetchUser = async () => {
+            const supabase = createClient();
+
+            const { data : { user } } = await supabase.auth.getUser();
+            const { data } = await supabase.from('profiles').select('id, name').maybeSingle();
+
+            if (user && user.id === data?.id) {
+                setCurrentUserName(data.name);
+            }
+        }
+        fetchUser()
+    }, [])
+
+    return(
+        <div className="h-dvh w-full min-w-0 overflow-x-hidden flex">
+            <div className="md:hidden flex flex-col w-full items-center">
+                <div className="w-full flex justify-between items-center mt-2 px-2">
+                    <h2>
+                        {currentUserName}
+                    </h2>
+                    <button className="">
+                        <Menu className="" />
+                    </button>
+                </div>
+            </div> 
+
+            {/* divider */}
+
+            <div>
+
+            </div>
         </div>
     )
 }
